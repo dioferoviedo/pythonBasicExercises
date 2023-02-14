9fae3861ad4a7664bb9f5fb731acf1d2d87fba5f
Diofer Oviedo <dioferteonilo@gmail.com>
2023-02-14 12:01:31 -0300

    Python Basic Exercises

---
+++ exercicio-fixacao-3.py
@@ -0,0 +1,26 @@
+print('Lista de convidados do Diofinho')
+print('##################################')
+
+quantia = input('Quantidade de convidados: ')
+lista_de_convidados = []
+
+
+i = 1
+while i <= int (quantia): # for i in range(int(quantia))
+    nome_convidado = input('Insira o nome do convidado #' + str(i) + ':')
+    lista_de_convidados.append(nome_convidado)
+    i += 1
+
+print('Serão', quantia, 'convidados.')
+
+print('\nLista de Convidados')
+for convidado in lista_de_convidados:
+    print(convidado)
+
+
+'''
+Faça um programa que leia a quantidade de pessoas que
+serão convidadas para uma festa. Após isso, o programa 
+irá perguntar o nome de todas as pessoas e colocar numa lista de convidados.
+Imprimir todos os nomes da lista.
+'''
